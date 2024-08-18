c8e9ed79a14d3a479657198be13bed974c3f2599
HoangDucBach <hoangbach0985@gmail.com>
2024-08-18 22:37:08 +0700

    First layout for Social Frontend

---
+++ frontend/config/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * @file constants.ts
+ * @summary Constants for the frontend, all the constants should be defined here
+ */
+
+export const API_URL = 'http://localhost:3000';
+export const APP_NAME = 'My App';
+export const APP_VERSION = '0.0.1';
+export const APP_DESCRIPTION = 'My App Description';
+export const APP_KEYWORDS = 'My App Keywords';
