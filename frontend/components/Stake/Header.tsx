ad9d5a6b86541522ca102aca335633c3c5e7523f
Tu Hoang Minh <tu.hoangminh@employmenthero.com>
2024-09-23 22:32:54 +0700

    merge staking page (#16)
    * fix: full width image at projects
    
    * add: staking page (basic ui)
    
    ---------
    
    Co-authored-by: hoangducach <hoangbach0985@gmail.com>

---
+++ frontend/components/Stake/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-semibold">Stake</h1>
+            <p className="text-base text-foreground">Stake protocol tokens to become a KOL, stake KOL tokens for private group access, and stake project tokens to join whitelists</p>
+        </div>
+    )
+}
+
+export default Header;
